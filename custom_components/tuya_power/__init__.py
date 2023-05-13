08c0a59bffe14eb9b9eadc4fc3cc7ecfa2d0e4b0
ptx58 <133464053+ptx58@users.noreply.github.com>
2023-05-13 10:29:31 +0800

    Update __init__.py

--- custom_components/tuya_power/__init__.py
+++ custom_components/tuya_power/__init__.py
@@ -1,1 +1,14 @@
-"""The tcp component."""
+"""
+Example custom integration for Home Assistant.
+
+For more details about this integration, please refer to the documentation at
+https://github.com/home-assistant/example-custom-config/tree/master/custom_components/my_custom_integration
+"""
+
+DOMAIN = "tuya_power"
+
+async def async_setup(hass, config):
+    """Set up the My Custom Integration component."""
+    # This method gets called when the integration is loaded. You can use it
+    # to set up any global state, or load any other dependencies.
+    return True
